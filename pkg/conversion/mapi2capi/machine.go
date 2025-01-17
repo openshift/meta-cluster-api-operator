openshift/cluster-capi-operator

mapi2capi: Add OpenStack implementation
Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

pkg/mapi2capi/machine.go
@@ -10,12 +10,16 @@ import (
 )
 
 const (
-	capiNamespace            = "openshift-cluster-api"
-	capiMachineAPIVersion    = "cluster.x-k8s.io/v1beta1"
-	capiMachineKind          = "Machine"
+	capiNamespace         = "openshift-cluster-api"
+	capiMachineAPIVersion = "cluster.x-k8s.io/v1beta1"
+	capiMachineKind       = "Machine"
+
 	workerUserDataSecretName = "worker-user-data"
-	awsTemplateAPIVersion    = "infrastructure.cluster.x-k8s.io/v1beta2"
-	awsTemplateKind          = "AWSMachineTemplate"
+
+	awsTemplateAPIVersion       = "infrastructure.cluster.x-k8s.io/v1beta2"
+	awsTemplateKind             = "AWSMachineTemplate"
+	openstackTemplateAPIVersion = "infrastructure.cluster.x-k8s.io/v1beta1"
+	openstackTemplateKind       = "OpenStackMachineTemplate"
 )
 
 // fromMachineToMachine translates a MAPI Machine to its Core CAPI Machine correspondent.

openshift/meta-cluster-api-operator

pkg/conversion/mapi2capi/machine.go
@@ -30,10 +30,14 @@ import (
 )
 
 const (
-	capiNamespace            = "openshift-cluster-api"
-	workerUserDataSecretName = "worker-user-data"
-	awsMachineKind           = "AWSMachine"
-	awsMachineTemplateKind   = "AWSMachineTemplate"
+	capiNamespace = "openshift-cluster-api"
+
+	workerUserDataSecretName     = "worker-user-data"
+
+	awsMachineKind               = "AWSMachine"
+	awsMachineTemplateKind       = "AWSMachineTemplate"
+	openstackMachineKind         = "OpenStackMachine"
+	openstackMachineTemplateKind = "OpenStackMachineTemplate"
 )
 
 var (